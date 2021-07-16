259d78cf8c51666bc93d3c59079a8e8543704c27
mahrusalimudin <83571985+mahrusalimudin@users.noreply.github.com>
2021-07-16 16:58:30 +0700

    projekuas

---
+++ src/config/FIREBASE/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAn8eWPuQ46hUqiiBK6CIPXD7M-prw1LCk',
+  authDomain: 'daftar-wisata.firebaseapp.com',
+  projectId: 'daftar-wisata',
+  storageBucket: 'daftar-wisata.appspot.com',
+  messagingSenderId: '209421909894',
+  appId: '1:209421909894:web:7746727e49187d3b3bf607',
+  measurementId: 'G-5Y2BYP54FN',
+});
+
+const FIREBASE = firebase;
+
+export default FIREBASE;
